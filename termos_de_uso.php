9f76638133d1118332055f0c7cfa845728375d12
PauloMiguel13000git config --global user.name PauloMiguel13000 <guell.28@hotmail.com>
2023-08-24 11:00:33 -0300

    calcIMC adicionado

---
+++ termos_de_uso.php
@@ -0,0 +1,12 @@
+<div class="modal-body">
+
+<form name="form1">
+
+
+<p>
+    <input type="checkbox" name="habi" id="habi" onClick="HabiDsabi()"> Eu li e aceito os termos de uso 
+    <a target="_blank" href="tos.html">termos de uso</a>.
+  </p>
+ <input type="button" class="btn btn-terciary"  name="envia" id="envia" value="Assinar" onclick="assinar()" disabled></form>
+<!-- fim do botão -->
+</div>
